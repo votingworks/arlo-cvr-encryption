379278772d0389e9865f07e9654e3422dfcc9e9e
Dan Wallach <dwallach@rice.edu>
2020-06-17 17:48:48 -0500

    removed tox; moved to makefile as used in electionguard-python
    also, now that electionguard-python is public, removed all the git submodule craziness

---
+++ Makefile
@@ -0,0 +1,96 @@
+.PHONY: all environment install install-mac install-linux install-windows lint validate test test-example coverage coverage-html coverage-xml coverage-erase
+
+CODE_COVERAGE ?= 90
+WINDOWS_32BIT_GMPY2 ?= packages/gmpy2-2.0.8-cp38-cp38-win32.whl
+WINDOWS_64BIT_GMPY2 ?= packages/gmpy2-2.0.8-cp38-cp38-win_amd64.whl
+OS ?= $(shell python -c 'import platform; print(platform.system())')
+IS_64_BIT ?= $(shell python -c 'from sys import maxsize; print(maxsize > 2**32)')
+
+all: environment install validate lint coverage
+
+environment:
+	@echo 🔧 PIPENV SETUP
+	pip install pipenv
+	pipenv install --dev
+
+install:
+	@echo 📦 Install Module
+	@echo Operating System identified as $(OS)
+ifeq ($(OS), Linux)
+	make install-linux
+endif
+ifeq ($(OS), Darwin)
+	make install-mac
+endif
+ifeq ($(OS), Windows)
+	make install-windows
+endif
+ifeq ($(OS), Windows_NT)
+	make install-windows
+endif
+
+install-mac:
+	@echo 🍎 MACOS INSTALL
+# gmpy2 requirements
+	brew install gmp || true
+# install module
+	pipenv run python -m pip install -e .
+
+install-linux:
+	@echo 🐧 LINUX INSTALL
+# gmpy2 requirements
+	sudo apt-get install libgmp-dev
+	sudo apt-get install libmpfr-dev
+	sudo apt-get install libmpc-dev
+# install module
+	pipenv run python -m pip install -e .
+
+install-windows:
+	@echo 🏁 WINDOWS INSTALL
+# install module with local gmpy2 package
+ifeq ($(IS_64_BIT), True)
+	pipenv run python -m pip install -f $(WINDOWS_64BIT_GMPY2) -e . 
+endif
+ifeq ($(IS_64_BIT), False)
+	pipenv run python -m pip install -f $(WINDOWS_32BIT_GMPY2) -e . 
+endif
+	
+
+lint:
+	@echo 💚 LINT
+	@echo 1.Pylint
+	pipenv run pylint .
+	@echo 2.Black Formatting
+	pipenv run black --check .
+	@echo 3.Mypy Static Typing
+	pipenv run mypy src tests setup.py
+	@echo 4.Package Metadata
+	pipenv run python setup.py check --strict --metadata --restructuredtext
+# 	@echo 5.Docstring
+# 	pipenv run pydocstyle
+
+validate: 
+	@echo ✅ VALIDATE
+	@pipenv run python -c 'import electionguard; print(electionguard.__package__ + " successfully imported")'
+
+test: 
+	@echo ✅ TEST
+	pipenv run pytest . -x
+
+test-example:
+	@echo ✅ TEST Example
+	pipenv run python -m pytest -s tests/integration/test_end_to_end_election.py
+
+coverage:
+	@echo ✅ COVERAGE
+	pipenv run coverage run -m pytest
+	pipenv run coverage report --fail-under=$(CODE_COVERAGE)
+
+coverage-html:
+	pipenv run coverage html -d coverage
+
+coverage-xml:
+	pipenv run coverage xml
+
+coverage-erase:
+	@pipenv run coverage erase
--- setup.py
+++ setup.py
@@ -13,81 +13,48 @@ from os.path import splitext
 from setuptools import find_packages
 from setuptools import setup
 
-
-# Borrowed from here: https://blog.ionelmc.ro/2014/05/25/python-packaging/
-
-
-def read(*names, **kwargs):
-    with io.open(
-        join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")
-    ) as fh:
-        return fh.read()
-
+NAME = "arlo-e2e"
+VERSION = "0.1.0"
+LICENSE = "GNU Affero General Public License v3"
+DESCRIPTION = "Arlo-e2e: Support for e2e verified risk-limiting audits."
+AUTHOR = "VotingWorks"
+AUTHOR_EMAIL = "dwallach@voting.works"
+URL = "https://github.com/votingworks/arlo-e2e"
+PROJECT_URLS = {
+    "Changelog": "https://github.com/votingworks/arlo-e2e/blob/master/CHANGELOG.rst",
+    "Issue Tracker": "https://github.com/votingworks/arlo-e2e/issues",
+}
+CLASSIFIERS = [
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    "Development Status :: 3 - Alpha",  # TODO Update when Stable
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU Affero General Public License v3",
+    "Operating System :: Unix",
+    "Operating System :: POSIX",
+    "Operating System :: MacOS",
+    "Operating System :: Microsoft :: Windows",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Utilities",
+]
 
 setup(
-    name="arlo-e2e",
-    version="0.1.0",
-    license="AGPLv3",
-    description="arlo-e2e: e2e verified risk-limiting audits",
-    # long_description='%s\n%s' % (
-    #     re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-    #     re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
-    # ),
-    author="Dan S. Wallach",
-    author_email="dwallach@voting.works",
-    url="https://github.com/votingworks/arlo-e2e",
-    packages=find_packages("src", "electionguard-python/src"),
-    package_dir={"": "src", "electionguard": "electionguard-python/src"},
+    name=NAME,
+    version=VERSION,
+    license=LICENSE,
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
-    classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Operating System :: Unix",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Python",
-        # 'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.5',
-        # 'Programming Language :: Python :: 3.6',
-        # 'Programming Language :: Python :: 3.7',
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: CPython",
-        # "Programming Language :: Python :: Implementation :: PyPy",
-        # uncomment if you test on these interpreters:
-        # 'Programming Language :: Python :: Implementation :: IronPython',
-        # 'Programming Language :: Python :: Implementation :: Jython',
-        # 'Programming Language :: Python :: Implementation :: Stackless',
-        "Topic :: Utilities",
-    ],
-    project_urls={
-        "Changelog": "https://github.com/votingworks/arlo-e2e/blob/master/CHANGELOG.rst",
-        "Issue Tracker": "https://github.com/votingworks/arlo-e2e/issues",
-    },
-    keywords=[
-        # eg: 'keyword1', 'keyword2', 'keyword3',
-    ],
+    classifiers=CLASSIFIERS,
+    project_urls=PROJECT_URLS,
     python_requires=">=3.8",
-    tests_require=["hypothesis==5.9.0"],
-    install_requires=[
-        "gmpy2==2.1.0b4",
-        "numpy==1.18.2",
-        "pandas==1.0.4",
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
-    ],
-    extras_require={
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
-    },
-    setup_requires=["pytest-runner",],
-    entry_points={
-        #         'console_scripts': [
-        #             'nameless = nameless.cli:main',
-        #        ]
-    },
+    install_requires=["gmpy2>=2.0.8"],
 )
