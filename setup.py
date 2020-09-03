05d309e646956504bdbcf5cbf5cbcf99175a9e83
Dan Wallach <dwallach@rice.edu>
2020-09-03 17:58:36 -0500

    squeezing out bugs in the setup (which also broke CI)

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     url=URL,
     packages=find_packages("src"),
     package_dir={"": "src"},
-    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=CLASSIFIERS,
