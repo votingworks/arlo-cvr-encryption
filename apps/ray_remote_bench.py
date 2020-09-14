7b58a0809ce9403e854e9ccf228c5a46f23bc3eb
Dan Wallach <dwallach@rice.edu>
2020-09-14 12:21:29 -0500

    remote bench how has --local flag, progress meter now suppresses mypy failure

--- apps/ray_remote_bench.py
+++ apps/ray_remote_bench.py
@@ -10,7 +10,7 @@ from electionguard.group import int_to_q_unchecked
 from electionguard.utils import get_optional
 
 from arlo_e2e.dominion import read_dominion_csv
-from arlo_e2e.ray_helpers import ray_init_cluster
+from arlo_e2e.ray_helpers import ray_init_cluster, ray_init_localhost
 from arlo_e2e.ray_tally import ray_tally_everything
 
 
@@ -55,11 +55,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Runs a tallying benchmark, using Ray (either locally or on a remote cluster)"
     )
-    # parser.add_argument(
-    #     "--cluster",
-    #     action="store_true",
-    #     help="uses a Ray cluster for distributed computation (local by default)",
-    # )
+    parser.add_argument(
+        "--local",
+        action="store_true",
+        help="uses Ray locally (Ray on a cluster by default)",
+    )
     parser.add_argument(
         "--dir",
         type=str,
@@ -78,7 +78,12 @@ if __name__ == "__main__":
     files = args.cvr_file
     file_dir = args.dir[0] if args.dir else None
 
-    ray_init_cluster()
+    if args.local:
+        print("Using Ray locally")
+        ray_init_localhost()
+    else:
+        print("Using Ray on a cluster")
+        ray_init_cluster()
 
     for arg in files:
         run_bench(arg, file_dir)
