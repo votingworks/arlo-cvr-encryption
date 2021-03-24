029aa5ab75f8abd6167efeb51225efdc76c6ac4c
Dan Wallach <dwallach@rice.edu>
2021-03-24 16:16:10 -0500

    updated versions for boto, etc.

--- typings/boto3/__main__.py
+++ typings/boto3/__main__.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
     print(
-        "Type annotations for boto3 1.17.27\n"
-        "Version:         1.17.27\n"
+        "Type annotations for boto3 1.17.36\n"
+        "Version:         1.17.36\n"
         "Builder version: 4.4.0\n"
         "Docs:            https://pypi.org/project/boto3-stubs/\n"
         "Changelog:       https://github.com/vemel/mypy_boto3_builder/releases"
